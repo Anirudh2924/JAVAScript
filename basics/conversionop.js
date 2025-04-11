165a45fd2168397f73f9f15cf99318c67101838d
Anirudh2924 <aniparihar2924@gmail.com>
2025-04-11 08:40:12 +0000

    operators

---
+++ basics/conversionop.js
@@ -0,0 +1,69 @@
+//let score = 33  // is in number
+/*
+let score = "33"
+let valuInNumber = Number(score)
+console.log(typeof score);
+console.log(typeof valuInNumber);
+
+this was all okay until we enter some alphanumerics in var 
+and convert then also it shows it got converted
+
+*/  
+
+
+/*
+let score = "33dfsd"
+let valuInNumber = Number(score)
+console.log(typeof score);
+console.log(typeof valuInNumber);
+console.log(valuInNumber); // in some cases this converts the alpha numeric
+//  codes but in some cases it does not change it , this is not any kind of bug but 
+// js tryies to convert the number somehow...!
+
+/*
+"33" => 33
+"33abs" => NaN
+true => 1; false=> 0
+*/
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true
+// "" => false
+//"Hitesh" =>true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+//console.log(someNumber);
+//console.log(typeof stringNumber);
+
+//***********************operations**************************/
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/2);
+console.log(2%2);
+*/
+
+let str1 = "hello"
+let str2 = "hitesh"
+
+let str3 = str1+str2
+/*
+console.log(str3);
+console.log("1"+"2");
+console.log(1+"2");
+console.log("1" + 2 + 2);
+console.log(2 + 2 + "1");
+*/
+
+
