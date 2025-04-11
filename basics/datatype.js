7846343bbb1b041fe854fefed07d92035749659d
Anirudh2924 <aniparihar2924@gmail.com>
2025-04-11 06:33:43 +0000

    datatypes

--- basics/datatype.js
+++ basics/datatype.js
@@ -2,3 +2,19 @@
 //alert() -  we are using node js not browser this is not available in js
 // in the same syntax 
 
+let name = "anyname" // string dt
+let age = "23" // integer dt
+let isLoggedIn = false // boolean dt
+
+// integer - 2^53 
+// bigint
+// string = " " , ' '
+//boolean -  true/flase
+// null - standalone value / empty value 
+// undefined - no value 
+// symbol - uniqueness to any component
+
+
+//object
+
+console.log(typeof undefined);
