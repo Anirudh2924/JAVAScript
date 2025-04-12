8e4750ba4608f352d1611bc5eaf714d7bed4e361
Anirudh2924 <aniparihar2924@gmail.com>
2025-04-12 09:10:16 +0000

    math and num

---
+++ basics/numnmath.js
@@ -0,0 +1,51 @@
+const score = 400
+//console.log(score);
+
+const balance = new Number(100)
+//console.log(balance);
+
+//console.log(balance.toString());
+//console.log(balance.toFixed(1));
+
+const otherNumber = 123.7654
+//console.log(otherNumber.toPrecision(4));
+
+const hundereds = 100000000000
+//console.log(hundereds.toLocaleString('en-IN'));
+
+
+// Mathsss.......!
+/*
+console.log(Math);
+
+console.log(Math.abs(-445)); // any -ve number turns +ve ,but +ve remains +ve
+console.log(Math.round(4.6)); // it will round of numbers as we use to do them in our earlier days >5 to ++ and <5 to --
+console.log(Math.ceil(4.2)); // in this the number will be rounded off to the top number , even if number is .01 above a number
+console.log(Math.floor(4.9999)); // in this number will be rounded of to the floor value of the number , even if it is 0.1 less than the next value...
+console.log(Math.min(4,1,5,8,3,7,6));
+console.log(Math.max(4,1,5,8,3,7,6));
+*/
+
+/*
+console.log(Math.random()+1);
+console.log((Math.random())*10 +1);
+*/
+//console.log(Math.floor((Math.random())*100) +1);
+
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1))+ min);
+
+
+///imp/////
+ 
+
+
+
+
+
+
+
+
