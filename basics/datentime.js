5cf3e3875fae46774a1be697170bd48429332c45
Anirudh2924 <aniparihar2924@gmail.com>
2025-04-12 09:29:32 +0000

    datentime

---
+++ basics/datentime.js
@@ -0,0 +1,15 @@
+let mydate = new Date()
+//console.log(mydate.toString());
+//console.log(mydate.toDateString());
+//console.log(mydate.toLocaleString());
+//console.log(typeof mydate);
+
+let myCreatedDate = new Date(2005 , 6 ,29)
+console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+
+
+
+
