0ebbedd19efce624aabcb6589fc943e88d15acfd
Anirudh2924 <aniparihar2924@gmail.com>
2025-04-13 05:39:57 +0000

    array

---
+++ basics/Array.js
@@ -0,0 +1,43 @@
+// Array
+
+const myArray = [0,1,2,3,4,5,6]
+const myalpha = ["a","b","c","d","e","f"]
+const myarr2 = new Array(1,2,3,4)
+//console.log(myArray[0]);
+
+
+//methods of array
+/*
+myArray.push(6) appends at last
+myArray.pop()  removes from last
+console.log(myArray);
+*/
+
+myArray.unshift(9) // adds elem at starting of the array 
+myArray.shift() // removes elem from the starting of an array
+//console.log(myArray.includes(9));
+//console.log(myArray.indexOf(12));
+
+/*
+const newArr =  myArray.join() // join binds the array and convert its data type to string
+console.log(myArray);
+console.log(newArr);
+*/
+
+
+// slice, splice
+console.log("A" ,myArray);
+const myn1 = myArray.slice(1,3)
+console.log(myn1);
+console.log("B",myArray);  
+
+
+
+
+
+
+
+
+
+
+
